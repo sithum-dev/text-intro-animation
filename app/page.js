84f412e31391c96cd271eb3d6fef7c4865a249ea
Sithum Imarsha <sithum.imarshadev@gmail.com>
2024-03-07 13:25:53 +0530

    ADD: fix animations loadings

--- app/page.js
+++ app/page.js
@@ -1,14 +1,29 @@
 "use client";
 
+import { useLayoutEffect, useState } from "react";
+import { gsap } from "gsap";
+
 import Loader from "@/components/Loader";
 import Hero from "@/components/Hero";
 
 const Home = () => {
+  const [loaderFinished, setLoaderFinished] = useState(false);
+  const [timeline, setTimeline] = useState(null);
+
+  useLayoutEffect(() => {
+    const context = gsap.context(() => {
+      const tl = gsap.timeline({
+        onComplete: () => setLoaderFinished(true),
+      });
+
+      setTimeline(tl);
+    });
+
+    return () => context.revert();
+  }, []);
+
   return (
-    <main>
-      <Hero />
-      <Loader />
-    </main>
+    <main>{loaderFinished ? <Hero /> : <Loader timeline={timeline} />}</main>
   );
 };
 
