b91dfcb722cf3d58d6d10f5c03b45ea3b5989481
Sithum Imarsha <sithum.imarshadev@gmail.com>
2024-03-10 19:00:57 +0530

    ADD: hero animation

--- app/components/Hero/index.js
+++ app/components/Hero/index.js
@@ -5,22 +5,44 @@ import Image from "next/image";
 import styles from "./Hero.module.scss";
 
 import Logo from "@/components/Logo";
+import { animateImage, animateTitle, revealMenu } from "./animations";
 
 const Hero = () => {
+  const timeline = useRef(gsap.timeline());
+  const heroRef = useRef(null);
+
+  useEffect(() => {
+    const context = gsap.context(() => {
+      const tl = timeline.current;
+
+      tl.add(animateTitle()).add(animateImage(), 0).add(revealMenu(), 0);
+    }, heroRef);
+
+    return () => context.revert();
+  }, []);
+
   return (
-    <section className={styles.hero}>
+    <section className={styles.hero} ref={heroRef}>
       <div className={styles.hero__top}>
-        <div data-menu-item>
+        <div data-menu-item data-hidden>
           <Logo />
         </div>
-        <span data-menu-item>about</span>
-        <span data-menu-item>contact</span>
+        <span data-menu-item data-hidden>
+          about
+        </span>
+        <span data-menu-item data-hidden>
+          contact
+        </span>
       </div>
 
       <h1 className={styles.hero__title}>
-        <span data-title-first>Ultra</span>
+        <span data-title-first data-hidden>
+          Ultra
+        </span>
         <span data-hero-line className={styles.hero__line}></span>
-        <span data-title-last>agency</span>
+        <span data-title-last data-hidden>
+          agency
+        </span>
       </h1>
 
       <div className={styles.hero__image}>
